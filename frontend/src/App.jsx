c7b01994c1e66fce1be92e8f411add3b90f775b5
srivyshnavi1505 <srivyshnavip1505@gmail.com>
2025-08-30 10:45:48 +0530

    Initial commit - React app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <div>
+          <Link to="/">SkillSwap</Link>
+        </div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
+      </nav>
+
+      <div className="page-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
